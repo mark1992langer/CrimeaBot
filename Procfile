5ef245a4d2f472fb6202b94731fec5b4807568db
mark1992langer <marklanger92@gmail.com>
2021-03-23 17:38:59 +0300

    prob

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS -jar target/CrimeaBot-1.0-SNAPSHOT-jar-with-dependencies.jar 
+web: java $JAVA_OPTS -Dserver.port=$PORT -jar target/CrimeaBot-1.0-SNAPSHOT-jar-with-dependencies.jar 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,13 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
 public class Main {
+    private static final String PORT = System.getenv("PORT");
 
     public static void main(String[] args) {
         ApiContextInitializer.init();
@@ -14,5 +20,14 @@ public class Main {
         }catch (TelegramApiException e){
             e.printStackTrace();
         }
+        try (ServerSocket serverSocket = new ServerSocket(Integer.valueOf(PORT))) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
